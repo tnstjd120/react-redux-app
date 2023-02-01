18461234b788ceea34c1492c11e3881fc2ee5570
soonsung <tnstjd120@gmail.com>
2023-02-01 10:49:41 +0900

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+type Props = {
+  value: number;
+  onIncrement: () => void;
+  onDecrement: () => void;
+}
+
+function App({ value, onIncrement, onDecrement }: Props) {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <button onClick={onIncrement}>+</button>
+      <span>{value}</span>
+      <button onClick={onDecrement}>-</button>
+
     </div>
   );
 }
